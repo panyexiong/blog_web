d22f5d69277b5cc8f2f13edc1e409e0e466708e7
panyexiong <panyexiong@xinye.com>
2020-11-17 19:50:49 +0800

    test workflows

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
         open: process.platform === "darwin",
         disableHostCheck: false,
         host: "0.0.0.0",
-        port: 8888,
+        port: 8899,
         https: false,
         hotOnly: false, // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
         proxy: null // string | Object
